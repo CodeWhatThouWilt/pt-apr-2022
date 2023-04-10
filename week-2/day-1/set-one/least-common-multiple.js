3826ac806b14f984717fbcc109711c5787613fbf
Jacob North <yakenorth@protonmail.com>
2023-04-10 19:30:03 -0400

    Added first set of practices week2 day1

---
+++ week-2/day-1/set-one/least-common-multiple.js
@@ -0,0 +1,9 @@
+// Write a function leastCommonMultiple(num1, num2) that accepts 
+// two numbers as arguments. The functions should return the 
+// smallest number that is divisible by both num1 and num2.
+
+
+
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
