d4347dee4a600fc0f8cfa81a7e4e3d7ce50a9693
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-04-13 14:00:17 -0700

    added while loop solution for helper-function problem

--- week-2/day-3/helper-functions.js
+++ week-2/day-3/helper-functions.js
@@ -1,12 +1,13 @@
 // Given an array of numbers, find the average of all the numbers
 
+// ======= FOR LOOP =======
 function findAvg(arr) {
-    
+
     const sum = findSum(arr);
 
     return sum / arr.length;
 }
-
+// For loop helper function
 function findSum(arr) {
     let sum = 0;
 
@@ -18,7 +19,30 @@ function findSum(arr) {
     return sum
 }
 
+// ======= WHILE LOOP =======
+function whileLoopFindAvg(arr) {
 
+    const sum = findSum(arr);
 
-console.log(findAvg([1,2,3,4,5]));
-console.log(findAvg([4,2,6,4,22,29]))
+    return sum / arr.length;
+}
+// While loop helper function
+function findSum(arr) {
+    let sum = 0;
+
+    let i = 0;
+    while (i < arr.length) {
+        const num = arr[i];
+		sum += num;
+        i += 1;
+    }
+    return sum
+}
+
+// ======= RUN TO TEST FOR LOOP =======
+// console.log(findAvg([1,2,3,4,5]));
+// console.log(findAvg([4,2,6,4,22,29]))
+
+// ======= RUN TO TEST WHILE LOOP =======
+console.log(whileLoopFindAvg([1,2,3,4,5]));
+console.log(whileLoopFindAvg([4,2,6,4,22,29]))
