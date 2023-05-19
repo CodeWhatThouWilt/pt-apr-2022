9636f1ef5ca3ea18cbc89b5d204a0f47b3305ff9
Jacob <yakenorth@gmail.com>
2023-05-18 21:07:14 -0400

    Added dragons solution week 7 day 4

--- east/week-7/day-4/dragons-classes/all-dragons.js
+++ east/week-7/day-4/dragons-classes/all-dragons.js
@@ -1,12 +1,38 @@
-// Your code here
+const EvilDragon = require("./classes/evil-dragon.js");
+const FriendlyDragon = require("./classes/friendly-dragon.js");
+const Dragon = require("./classes/dragon.js");
 
+const falkor = new FriendlyDragon(
+	"Falkor",
+	"white",
+	[
+		"save Atreyu from the swamp",
+		"save Atreyu from the Nothing",
+		"scare the local bullies into a dumpster",
+	],
+	"Bastian"
+);
+
+const smaug = new EvilDragon(
+	"Smaug",
+	"black",
+	[
+		"take over your mountain kingdom",
+		"steal all your dwarven gold",
+		"burn down your floating village",
+	],
+	"Dwarf King"
+);
+
+const allDragons = Dragon.getDragons(falkor, smaug);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports.falkor = falkor
-  module.exports.smaug = smaug
+	module.exports.falkor = falkor;
+	module.exports.smaug = smaug;
+	module.exports.allDragons = allDragons;
 } catch {
-  module.exports = null;
+	module.exports = null;
 }
