f17b696921d085c066e1a75d6202082a9ef675d5
Jacob <yakenorth@gmail.com>
2023-05-23 17:41:39 -0400

    Added inital setup for week-8 day-2 content

---
+++ east/week-8/day-2/bind.js
@@ -0,0 +1,40 @@
+class Book {
+	constructor(author, title) {
+		this.author = author;
+		this.title = title;
+	}
+
+	read() {
+		console.log(`${this.title} by ${this.author} is being read.`);
+	}
+
+	arrowFunc() {
+		return () => {
+			console.log(this);
+		};
+	}
+
+	regularFunc() {
+		return function () {
+			console.log(this);
+		};
+	}
+
+	withTimeoutArrow() {
+		setTimeout(() => {
+			console.log(this);
+		});
+	}
+
+	withTimeoutReg() {
+		setTimeout(function () {
+			console.log(this);
+		});
+	}
+}
+
+const newBook = new Book("The Odyssey", "Homer");
+
+function test() {
+	console.log(this);
+}
