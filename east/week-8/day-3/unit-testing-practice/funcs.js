462f3e8ef36cbf4e4fd12752a8bfec9c140367c0
Jacob <yakenorth@gmail.com>
2023-05-24 23:10:33 -0400

    Cheatsheets fix

--- east/week-8/day-3/unit-testing-practice/funcs.js
+++ east/week-8/day-3/unit-testing-practice/funcs.js
@@ -1,14 +1,17 @@
 function isFive(num) {
-  // Your code here
+	return num === 5 ? true : false;
 }
 
 function isOdd(number) {
-  // Your code here
+	// Your code here
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+	const arr = [];
+	for (let i = min; i <= max; i += step) {
+		arr.push(i);
+	}
+	return arr;
 }
-
 
 module.exports = { isFive, isOdd, myRange };
--- east/week-8/day-3/unit-testing-practice/test/class-spec.js
+++ east/week-8/day-3/unit-testing-practice/test/class-spec.js
@@ -4,31 +4,33 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
-  describe("Word constructor function", function () {
-    it('should have a "word" property', function () {
-      expect.fail("replace with your code");
-    });
-  
-    it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
-    });
-  });
+	const newWord = new Word("hello");
 
-  describe("removeVowels function", function () {
-    it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
-    });
-  });
+	describe("Word constructor function", function () {
+		it('should have a "word" property', function () {
+			expect(newWord.word).to.not.equal(undefined);
+		});
 
-  describe("removeConsonants function", function () {
-    it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
-    });
-  });
-  
-  describe("pigLatin function", function () {
-    it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
-    });
-  });
+		it('should set the "word" property when a new word is created', function () {
+			expect(newWord.word).to.equal("hello");
+		});
+	});
+
+	describe("removeVowels function", function () {
+		it("should return the word with all vowels removed", function () {
+			expect(newWord.removeVowels()).to.equal("hll");
+		});
+	});
+
+	describe("removeConsonants function", function () {
+		it("should return the word with the consonants removed", function () {
+			expect.fail("replace with your code");
+		});
+	});
+
+	describe("pigLatin function", function () {
+		it("should return the word converted to pig latin", function () {
+			expect.fail("replace with your code");
+		});
+	});
 });
