59d18fb075a19acf6e4eaed6ae94d3fad90a5937
Jacob <yakenorth@gmail.com>
2023-05-27 16:59:05 -0400

    Added dynamic validate code

--- practice-assessments/weeks-7-8/class/vehicle.js
+++ practice-assessments/weeks-7-8/class/vehicle.js
@@ -31,6 +31,39 @@ class Vehicle {
 			this.price = price;
 		}
 	}
+
+	// ** Dynamic validate, once again, not recommended
+	// validate(year, price, isUpdate) {
+	// 	const checkData = this.modelName && this.year && this.price;
+	// 	const checkYear = this.year >= 1950 && this.year <= 2100;
+
+	// 	if (isUpdate) {
+	// 		const checkPrice = price > 0;
+	// 		const checkYear = year >= 1950 && year <= 2100;
+
+	// 		if (!checkPrice) {
+	// 			throw new Error("Price must be greater than 0");
+	// 		} else if (!checkYear) {
+	// 			throw new Error("Year must be between 1950 and 2100");
+	// 		} else {
+	// 			return true;
+	// 		}
+	// 	} else {
+	// 		if (checkData && checkYear) {
+	// 			return true;
+	// 		} else {
+	// 			return false;
+	// 		}
+	// 	}
+	// }
+
+	// update(modelName, year, price) {
+	// 	if (this.validate(year, price, true)) {
+	// 		this.modelName = modelName;
+	// 		this.year = year;
+	// 		this.price = price;
+	// 	}
+	// }
 }
 
 module.exports = Vehicle;
