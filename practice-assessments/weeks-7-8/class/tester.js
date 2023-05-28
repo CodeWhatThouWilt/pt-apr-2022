627945d652efa5f3f39d34c0fb1335a8ed42909c
Jacob <yakenorth@gmail.com>
2023-05-27 20:57:44 -0400

    PA solution added

--- practice-assessments/weeks-7-8/class/tester.js
+++ practice-assessments/weeks-7-8/class/tester.js
@@ -1,3 +1,28 @@
-class Tester {}
+const Review = require("./review.js");
+const Bicycle = require("./bicycle.js");
+const ElectricBicycle = require("./electric-bicycle.js");
 
+class Tester {
+	constructor(name) {
+		this.name = name;
+		this.reviews = [];
+		this.bikeTester;
+		this.eBikeTester;
+	}
+
+	submitReview(vehicle, starRating, text) {
+		if (vehicle.validate()) {
+			const newReview = new Review(vehicle, this, starRating, text);
+			newReview.addReview();
+			if (vehicle instanceof ElectricBicycle) {
+				this.eBikeTester = true;
+			} else if (vehicle instanceof Bicycle) {
+				this.bikeTester = true;
+			}
+			return newReview;
+		} else {
+			throw new Error("Cannot submit review for invalid vehicle.");
+		}
+	}
+}
 module.exports = Tester;
--- practice-assessments/weeks-7-8/class/vehicle.js
+++ practice-assessments/weeks-7-8/class/vehicle.js
@@ -32,38 +32,46 @@ class Vehicle {
 		}
 	}
 
-	// ** Dynamic validate, once again, not recommended
-	// validate(year, price, isUpdate) {
-	// 	const checkData = this.modelName && this.year && this.price;
-	// 	const checkYear = this.year >= 1950 && this.year <= 2100;
+	getDetails() {
+		return `The ${this.year} ${this.modelName} costs $${this.price} and has ${this.reviews.length} reviews.`;
+	}
 
-	// 	if (isUpdate) {
-	// 		const checkPrice = price > 0;
-	// 		const checkYear = year >= 1950 && year <= 2100;
-
-	// 		if (!checkPrice) {
-	// 			throw new Error("Price must be greater than 0");
-	// 		} else if (!checkYear) {
-	// 			throw new Error("Year must be between 1950 and 2100");
-	// 		} else {
-	// 			return true;
-	// 		}
-	// 	} else {
-	// 		if (checkData && checkYear) {
-	// 			return true;
-	// 		} else {
-	// 			return false;
-	// 		}
-	// 	}
-	// }
-
-	// update(modelName, year, price) {
-	// 	if (this.validate(year, price, true)) {
-	// 		this.modelName = modelName;
-	// 		this.year = year;
-	// 		this.price = price;
-	// 	}
-	// }
+	findReviewByTester(name) {
+		return this.reviews.find((review) => review.tester.name === name);
+	}
 }
 
 module.exports = Vehicle;
+
+// // ** Dynamic validate, once again, not recommended
+// validate(year, price, isUpdate) {
+// 	const checkData = this.modelName && this.year && this.price;
+// 	const checkYear = this.year >= 1950 && this.year <= 2100;
+
+// 	if (isUpdate) {
+// 		const checkPrice = price > 0;
+// 		const checkYear = year >= 1950 && year <= 2100;
+
+// 		if (!checkPrice) {
+// 			throw new Error("Price must be greater than 0");
+// 		} else if (!checkYear) {
+// 			throw new Error("Year must be between 1950 and 2100");
+// 		} else {
+// 			return true;
+// 		}
+// 	} else {
+// 		if (checkData && checkYear) {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+// }
+
+// update(modelName, year, price) {
+// 	if (this.validate(year, price, true)) {
+// 		this.modelName = modelName;
+// 		this.year = year;
+// 		this.price = price;
+// 	}
+// }
