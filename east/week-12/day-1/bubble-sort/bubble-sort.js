f231b010d9bf6f32f6c9483c3ba168b5934a71df
Jacob <yakenorth@gmail.com>
2023-06-19 20:55:01 -0400

    week-12 day-1 east solutions

--- east/week-12/day-1/bubble-sort/bubble-sort.js
+++ east/week-12/day-1/bubble-sort/bubble-sort.js
@@ -1,18 +1,25 @@
-
 function bubbleSort(arr) {
-
-    // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+	let swapped = false;
+	// Iterate through the array
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			// If the current value is greater than its neighbor to the right
+			// Swap those values
+			if (arr[j] > arr[j + 1]) {
+				// [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				// Do not move this console.log
+				console.log(arr.join(","));
+				swapped = true;
+			}
+		}
+		// If you get to the end of the array and no swaps have occurred, return
+		if (!swapped) return;
+		swapped = false;
+		// Otherwise, repeat from the beginning
+	}
 }
 
 module.exports = bubbleSort;
--- east/week-12/day-1/selection-sort/selection-sort.js
+++ east/week-12/day-1/selection-sort/selection-sort.js
@@ -1,46 +1,64 @@
-
-
 function selectionSort(arr) {
+	// Copy the original array
+	const copy = [...arr];
+	// Create an array to store the sorted values
+	const sorted = [];
+	// While the array is not empty...
+	while (copy.length) {
+		// Do not move this console.log
+		console.log(sorted.join(","));
 
-  // Copy the original array
+		// Find the index of the minimum value in the unsorted half
+		// Save and remove the value at the min index
+		// const index = copy.reduce((acc, num, i) => (num < copy[acc] ? i : acc), 0);
+		// const smallestNum = copy[index];
+		let idx = 0;
+		let min = copy[0];
+		for (let i = 0; i < copy.length; i++) {
+			const num = copy[i];
+			if (num < min) {
+				idx = i;
+				min = num;
+			}
+		}
 
-  // Create an array to store the sorted values
+		copy.splice(idx, 1);
 
-  // While the array is not empty...
-
-    // Do not move this console.log
-    console.log(sorted.join(","));
-
-    // Find the index of the minimum value in the unsorted half
-
-    // Save and remove the value at the min index
-
-    // Add the min value to the end of the sorted array
-
+		// Add the min value to the end of the sorted array
+		sorted.push(min);
+	}
+	return sorted;
 }
-
-
 
 function selectionSortInPlace(arr) {
+	// Set a pointer at zero dividing the array into sorted and unsorted halves
+	let pointer = 0;
+	// Repeat while the unsorted half is not empty:
+	while (pointer < arr.length) {
+		// Do not move this console.log
+		console.log(arr.join(","));
 
-  // Set a pointer at zero diving the array into sorted and unsorted halves
+		// Find the index of the minimum value in the unsorted half
+		let idx = pointer;
+		// Save the min value
+		let min = arr[pointer];
+		for (let i = pointer + 1; i < arr.length; i++) {
+			const num = arr[i];
+			if (num < min) {
+				idx = i;
+				min = num;
+			}
+		}
 
-  // Repeat while the unsorted half is not empty:
-
-    // Do not move this console.log
-    console.log(arr.join(","));
-
-    // Find the index of the minimum value in the unsorted half
-
-    // Save the min value
-
-    // Shift every unsorted value to the left of the min value to the right by 1
-
-    // Put the min value at the divider
-
-    // Increment the divider and repeat
-
+		// Shift every unsorted value to the left of the min value to the right by 1
+		for (let i = idx; i > pointer; i--) {
+			arr[i] = arr[i - 1];
+		}
+		// Put the min value at the divider
+		arr[pointer] = min;
+		// Increment the divider and repeat
+		pointer++;
+	}
 }
-
 
 module.exports = [selectionSort, selectionSortInPlace];
