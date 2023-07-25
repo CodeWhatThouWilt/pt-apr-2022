537c81181e537b6231b63ad6ab5fa0999859f740
Jacob <yakenorth@gmail.com>
2023-07-24 20:05:54 -0400

    week-16 day-1 promise all

---
+++ east/week-16/day-1/promise-all/phase-1.js
@@ -0,0 +1,61 @@
+function stretch() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log("done stretching");
+			resolve();
+		}, 1000);
+	});
+}
+
+function runOnTreadmill() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log("done running on treadmill");
+			reject("Tripped on the treadmill. Ouch!");
+		}, 500);
+	});
+}
+
+function liftWeights() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log("done lifting weights");
+			resolve();
+		}, 2000);
+	});
+}
+
+function workout() {
+	// refactor this code to use Promise.all
+
+	// const promise1 = stretch();
+	// const promise2 = runOnTreadmill();
+	// const promise3 = liftWeights();
+
+	// Promise.all([promise1, promise2, promise3])
+	// 	.then(() => console.log("done working out"))
+	// 	.catch((err) => console.log(err));
+
+	return Promise.all([stretch(), runOnTreadmill(), liftWeights()])
+		.then(() => console.log("done working out"))
+		.catch((err) => console.log("Oooooooof"));
+
+	// stretch()
+	//   .then(runOnTreadmill)
+	//   .then(liftWeights)
+	//   .then(() => console.log("done working out"))
+	//   .catch((err) => console.log(err));
+}
+
+/* ============================ TEST YOUR CODE ============================
+
+Run the file (`node phase-1.js`) and check your output against the expected
+output.
+*/
+
+workout();
+// should print out the following:
+// done running on treadmill
+// done stretching
+// done lifting weights
+// done working out
