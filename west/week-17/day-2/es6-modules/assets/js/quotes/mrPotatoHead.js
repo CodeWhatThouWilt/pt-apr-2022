fc736160b75ed344f8f3070f5f4e2a4130cb1127
Jacob <yakenorth@gmail.com>
2023-08-02 00:03:59 -0400

    es6 modules west

---
+++ west/week-17/day-2/es6-modules/assets/js/quotes/mrPotatoHead.js
@@ -0,0 +1,6 @@
+export const mrPotatoHeadQuotes = {
+	hello: "Hi, I'm Mr. Potato Head!",
+	bye: "Bye, it's been nice talking to you!",
+};
+
+// export const yake = "hello";
---
+++ west/week-17/day-2/es6-modules/assets/js/utilities.js
@@ -0,0 +1,8 @@
+export const getIndex = () => {
+	const wrapper = document.getElementById("wrapper");
+	return parseInt(wrapper.dataset.index);
+};
+
+// export {
+//     getIndex
+// }
