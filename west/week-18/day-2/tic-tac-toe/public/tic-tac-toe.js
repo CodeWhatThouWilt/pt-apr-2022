532c0b00ad88e51d3e3fa6db3f5a44e1deaf6d91
Jacob <yakenorth@gmail.com>
2023-08-10 01:25:29 -0400

    week 18 day 2 west tic tac toe partial

--- west/week-18/day-2/tic-tac-toe/public/tic-tac-toe.js
+++ west/week-18/day-2/tic-tac-toe/public/tic-tac-toe.js
@@ -1,11 +1,14 @@
-// Your code here
-
 window.onload = () => {
 	const gameBoard = document.getElementById("game-board");
+	gameBoard.style.display = "none";
+	let playerTurn = "x";
 
 	gameBoard.addEventListener("click", (e) => {
-		e.target.classList.add("x");
+		e.target.classList.add(playerTurn);
 		console.log(e.target);
+		playerTurn = playerTurn === "x" ? "o" : "x";
 		console.log(e.target.classList.contains("x"));
 	});
 };
+
+window.addEventListener("DOMContentLoaded", () => {});
