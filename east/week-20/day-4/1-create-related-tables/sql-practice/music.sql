76b9fdb7fc638dd65a264a35404d29fd17f95957
Jacob <yakenorth@gmail.com>
2023-08-24 19:17:47 -0400

    week-20 day-4 east practice-1

---
+++ east/week-20/day-4/1-create-related-tables/sql-practice/music.sql
@@ -0,0 +1,31 @@
+-- This ensures that SQLite enforces FOREIGN KEY constraints
+PRAGMA foreign_keys = 1;
+DROP TABLE IF EXISTS instruments;
+DROP TABLE IF EXISTS musicians;
+DROP TABLE IF EXISTS bands;
+
+CREATE TABLE bands (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(100)
+);
+
+CREATE TABLE musicians (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100),
+  band_id INTEGER,
+  FOREIGN KEY (band_id) REFERENCES bands(id)
+);
+
+CREATE TABLE instruments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE musician_instruments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  musician_id INTEGER,
+  instrument_id INTEGER,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id),
+  FOREIGN KEY (instrument_id) REFERENCES instruments(id)
+);
