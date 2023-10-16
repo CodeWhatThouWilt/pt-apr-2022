44eb019f73e9eb81f04c71548319bffdb04c283d
Jacob North <yakenorth@gmail.com>
2023-10-16 18:53:29 -0400

    week 28 day 1 opening discussion east

--- east/week-28/day-1/opening-discussion/src/FormIntro.js
+++ east/week-28/day-1/opening-discussion/src/FormIntro.js
@@ -1,5 +1,67 @@
+import { useEffect, useState } from "react";
+
 const FormIntro = () => {
-  return <div>FormIntro</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleUsername = (e) => {
+    const splitUsername = e.target.value.split(" ");
+    const joinedUsername = splitUsername.join("-");
+    setUsername(joinedUsername);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors([]);
+    const currErrors = [];
+
+    if (username.length === 0) currErrors.push("Username required");
+
+    if (currErrors.length) {
+      setErrors(currErrors);
+      return;
+    }
+
+    const payload = { username, password };
+
+    console.log(payload);
+  };
+
+  useEffect(() => {
+    setErrors([]);
+    const currErrors = [];
+
+    if (password.length === 0) currErrors.push("Password required");
+    if (password.length > 20) currErrors.push("Password too long");
+
+    setErrors(currErrors);
+  }, [password]);
+
+  return (
+    <div>
+      <ul>
+        {errors.map((error, i) => (
+          <li key={i}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input value={username} onChange={handleUsername} />
+        </label>
+
+        <label>
+          Password
+          <input value={password} onChange={handlePassword} />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormIntro;
