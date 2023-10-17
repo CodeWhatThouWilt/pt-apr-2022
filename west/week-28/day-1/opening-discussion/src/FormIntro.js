a766a706ba25dae70786202c3efe18c2d6fd2533
Jacob North <yakenorth@gmail.com>
2023-10-16 23:00:56 -0400

    week 28 day 1 opening discussion code east

--- west/week-28/day-1/opening-discussion/src/FormIntro.js
+++ west/week-28/day-1/opening-discussion/src/FormIntro.js
@@ -1,5 +1,61 @@
+import { useState, useEffect } from "react";
+
 const FormIntro = () => {
-  return <div>FormIntro</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleUsername = (e) => {
+    const splitUsername = e.target.value.split(" ");
+    const joined = splitUsername.join("-");
+    setUsername(joined);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const currErrors = [];
+
+    if (!username.length) currErrors.push("Username required");
+
+    if (currErrors.length) {
+      setErrors(currErrors);
+      return;
+    }
+
+    const payload = { username, password };
+    console.log("SENDING TO THE SERVER", payload);
+  };
+
+  useEffect(() => {
+    const currErrors = [];
+
+    if (!password.length) currErrors.push("Password required");
+    if (password.length > 20) currErrors.push("Password too long");
+
+    setErrors(currErrors);
+  }, [password]);
+
+  return (
+    <div>
+      <ul>
+        {errors.map((error, i) => (
+          <li key={i}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input value={username} onChange={handleUsername} />
+        </div>
+        <div>
+          <label>Password</label>
+          <input value={password} onChange={handlePassword} />
+        </div>
+        <button disabled={errors.length}>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormIntro;
